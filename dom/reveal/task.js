4bf610673833652ece20e9b156abaae885166fca
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-09-30 11:55:13 +0800

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,28 @@
+let listItems = document.querySelectorAll(".reveal");
+
+const isInViewport = function (element) {
+	const viewportHeight = window.innerHeight;
+	const elementTop = element.getBoundingClientRect().top;
+	const elementHeight = element.getBoundingClientRect().height;
+	return elementTop < viewportHeight && elementTop > -elementHeight ?
+		true :
+		false;
+};
+
+function checkReveals() {
+  [...listItems].forEach(element => {
+		if (isInViewport(element)) {
+			if (!element.classList.contains("reveal_active")) {
+				element.classList.add("reveal_active");
+			}
+		} else {
+			if (element.classList.contains("reveal_active")) {
+				element.classList.remove("reveal_active");
+			}
+		}
+	});
+}
+
+window.addEventListener("scroll", checkReveals, false);
+
+checkReveals();
